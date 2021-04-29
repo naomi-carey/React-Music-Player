249c4708c472619831c16779ef7642d4ad6fa420
Naomi Carey <naomicarey00@gmail.com>
2021-04-29 18:02:33 +0200

    adding styles for player

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
+import data from "./util";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
